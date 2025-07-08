9493bdfa65e9fe1ab05fc9083c37daf0cd332e4c
Eorico Gonzales <eoricogonzales@gmail.com>
2025-07-08 18:00:02 +0800

    Update Vercel

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://finance-web-steel.vercel.app/api/auth';
 
 const apiClient = axios.create({
   baseURL: API_URL,
